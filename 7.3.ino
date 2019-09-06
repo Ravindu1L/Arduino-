5f5be37b1db3b34ba8f8cd504300a8cc3b07283d
Ravindu1L <53072068+Ravindu1L@users.noreply.github.com>
2019-09-06 23:46:58 +1000

    Update 7.3.ino

--- 7.3.ino
+++ 7.3.ino
@@ -1,16 +1,18 @@
-int buzzerPin = 8,7;                          //BUZZER INPUT PIN.
-int ledPin = 13,12;                            //LED PIN.
-int pirPin1 = A0;                            //MOTION SENSOR INPUT PIN.
-int sensor1Value = LOW;                      //DEFAULT SENSOR STATE.
+int buzzerPin = 8,7;                          
+int ledPin = 13,12;                           
+int pirPin1 = A0; 
+int pirPin2 = A1;
+int sensor1Value = LOW;                     
 int sensor2Value = LOW;
 
-void setup() {                              //INITIAL SETTINGS/ASSIGNMETN.
-  pinMode(buzzerPin, OUTPUT);               //SET BUZZER AS OUTPUT.
-  pinMode(ledPin, OUTPUT);                  //SET LED AS OUTPUT.
-  pinMode(pirPin, INPUT);                   //SET PIR AS INPUT.
+void setup() {                              
+  pinMode(buzzerPin, OUTPUT);               
+  pinMode(ledPin, OUTPUT);                  
+  pinMode(pirPin1, INPUT);
+  pinMode(pirPin2, INPUT);
 }
-void loop() {                               //COMMAND TO BE REPEATED.
-  sensor1Value = digitalRead(pirPin1);        //READ PIR INPUT PIN.
+void loop() {                               
+  sensor1Value = digitalRead(pirPin1);        
   sensor2Value = digitalRead(pirPin2);
   if ( sensor1Value == HIGH)and (sensor2Value == HIGH) {               
     tone(buzzerPin, 1200);                   
